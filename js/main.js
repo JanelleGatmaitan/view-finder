6c2719d53fc8f55d55a460bed3861e25c3449db1
Janelle Gatmaitan <janellegatmaitan@gmail.com>
2021-02-25 22:50:10 -0800

    photoUrls match place #3

--- js/main.js
+++ js/main.js
@@ -103,10 +103,6 @@ function getPlacesData() {
       searchData.placesSearchResults.push(place);
       getPlacesPhotoData(searchData.placesSearchResults[i]);
     }
-    // for (var i = 0; i < searchData.placesSearchResults.length; i++) {
-    //   getPlacesPhotoData(searchData.placesSearchResults[i]);
-    // }
-    // storePhotoData();
   });
   xhr.send();
 }
@@ -122,24 +118,18 @@ function getPlacesData() {
 //     searchData.placesSearchResults[i].photoUrl = photoURL;
 //   }
 // }
-var index = 0;
 function getPlacesPhotoData(result) {
   var xhr = new XMLHttpRequest();
   var photoEndpoint = 'https://api.foursquare.com/v2/venues/';
   var clientID = '/photos?client_id=MGLS4THDKMFHYLSPVD5FIA1QNNUYFTSLERRRYZYKOWPKVK2R&client_secret=VZAFPX0YYBAAZ0GGDPMAR2VAJR5CFBWQXTXQ5IUBBF4H521E&v=20180323&group=venue&limit=1';
-  // xhr.open('GET', photoEndpoint + result.id + clientID);
   xhr.open('GET', photoEndpoint + result.id + clientID);
   xhr.responseType = 'json';
   xhr.addEventListener('load', function () {
     console.log(xhr.response);
-
-    // var prefix = searchData.photoData[i].response.photos.items[0].prefix;
-    // var suffix = searchData.photoData[i].response.photos.items[0].suffix;
     var prefix = xhr.response.response.photos.items[0].prefix;
     var suffix = xhr.response.response.photos.items[0].suffix;
     var photoURL = prefix + '500x500' + suffix;
-    searchData.placesSearchResults[index].photoUrl = photoURL;
-    index++;
+    searchData.placesSearchResults[searchData.placesSearchResults.indexOf(result)].photoUrl = photoURL;
   });
   xhr.send();
 }
